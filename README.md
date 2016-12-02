bf005fd51aaed5898b2a3b9cd47fe9ffe1a5cd8d
Ivan Koptiev <ikoptev@gmail.com>
2016-12-02 13:24:12 +0200

    First version

--- README.md
+++ README.md
@@ -1,2 +1,43 @@
-# winston-transport-stackdriver-error-reporting
-Stackdriver Error Reporting transport for winston logger (Node.js)
+Stackdriver Error Reporting transport for Winston logger
+========================================================
+
+
+Overview
+--------
+
+This package is [Winston] logger transport that sends errors to [Stackdriver Error Reporting] service.
+
+It's based on [@google/cloud-errors] library.
+
+
+Usage and Configuration
+-----------------------
+
+Simply add this to your Winston transports array:
+
+```js
+    winston.configure({
+      transports: [
+        new StackdriverErrorReporting(options),
+      ],
+    });
+```
+
+This transport supports 100% same configuration options as [@google/cloud-errors] library.
+The only additional option is `options.level` that controls minimal log entry level that should be sent to Stackdriver.
+
+
+### How it works
+
+When log entry reaches this transport, it will search `meta` object of log entry for objects that looks like errors
+(with `message` and `stack` properties defined).
+After that, all errors will be delivered to [Stackdriver Error Reporting] service.
+
+If `meta.request` property is defined, it will be attached to error as it's context.
+
+
+[//]: # (These are reference links used in the body of this document)
+
+[Winston]: <https://github.com/winstonjs/winston>
+[Stackdriver Error Reporting]: <https://cloud.google.com/error-reporting/>
+[@google/cloud-errors]: <https://github.com/GoogleCloudPlatform/cloud-errors-nodejs>
---
+++ src/index.js
@@ -0,0 +1,92 @@
+import forOwn from 'for-own'
+import isPlainObject from 'is-plain-object'
+import errorReporter from '@google/cloud-errors'
+import {Transport} from 'winston/lib/winston/transports/transport'
+
+/**
+ * Winston transport for Stackdriver Error Reporting service.
+ */
+class StackdriverErrorReporting extends Transport {
+
+  /**
+   * Class constructor.
+   *
+   * @param {Object} options Configuration options.
+   */
+  constructor(options = {}) {
+
+    super(options);
+
+    this.name = 'StackdriverErrorReporting';
+    this.level = options.level || 'error';
+
+    // NODE_ENV hack is required until
+    // https://github.com/GoogleCloudPlatform/cloud-errors-nodejs/issues/79
+    // will be resolved
+    const previousNodeEnv = process.env.NODE_ENV;
+    process.env.NODE_ENV = 'production';
+    this._client = errorReporter.start(options);
+    process.env.NODE_ENV = previousNodeEnv;
+  }
+
+  /**
+   * Send entry to Stackdriver Error Reporting.
+   *
+   * @param {String} level - Winston log entry level.
+   * @param {String} message - Log entry message.
+   * @param {Object} meta - Winston meta information.
+   * @param {Function} callback - Callback to let Winston know we are done.
+   */
+  log(level, message, meta, callback) {
+
+    const promises = [];
+    const errors = this.extractErrorsFromMeta(meta);
+
+    errors.forEach((error) => {
+      promises.push(
+        new Promise((resolve, reject) => {
+          this._client.report(error, meta.request, error.stack, (reportError) => {
+            if (reportError) {
+              console.log('Failed to send error to Stackdriver Error Reporting.', reportError);
+              return reject(reportError);
+            }
+            return resolve();
+          })
+        })
+      );
+    });
+
+    Promise.all(promises)
+      .then(() => callback(null, true))
+      .catch((error) => callback(error, false));
+  }
+
+  /**
+   * Extracts all errors from Winston metadata.
+   *
+   * @param {Object} data - Winston meta data.
+   * @return {Array}
+   */
+  extractErrorsFromMeta(data) {
+
+    const errors = [];
+
+    if (isPlainObject(data)) {
+      if (data.message && data.stack) {
+        errors.push(data);
+      } else {
+        forOwn(data, (value) => {
+          errors.concat(this.extractErrorsFromMeta(value));
+        });
+      }
+    } else if (Array.isArray(data)) {
+      data.forEach((value) => {
+        errors.concat(this.extractErrorsFromMeta(value));
+      });
+    }
+
+    return errors;
+  }
+}
+
+export default StackdriverErrorReporting
